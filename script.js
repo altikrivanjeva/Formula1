784a050b71270c29694ed8008dba7c39aab02b74
altikrivanjeva <altikrivanjeva@gmail.com>
2024-01-24 17:45:58 +0100

    Update i home page dhe teams

--- script.js
+++ script.js
@@ -18,7 +18,36 @@ function changeSlide(n) {
     showSlide(currentSlide + n);
 }
 
-// Auto advance slides every 3 seconds
 setInterval(() => {
     changeSlide(1);
 }, 5000);
+
+
+// Timer
+var countDownDate = new Date("Feb 28, 2024 10:00:00").getTime();
+
+
+var x = setInterval(function() {
+
+
+  var now = new Date().getTime();
+    
+
+  var distance = countDownDate - now;
+    
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
